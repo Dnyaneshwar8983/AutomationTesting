166cfccabf2b7819e172ded3b7b05c66a5e944a6
ADMIN <ADMIN@DESKTOP-58F0PJV>
2024-11-01 14:06:07 +0530

    compny project

---
+++ company/src/main/java/com/test/Department.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Department {
+
+	public static void main(String[] args) {
+		System.out.println("Testing Department");
+		System.out.println("HR Department");
+		System.out.println("Development Department");
+        System.out.println("IT Department");
+		
+	}
+
+}
