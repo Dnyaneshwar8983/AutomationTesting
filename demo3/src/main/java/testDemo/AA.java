ca18e6d198e09b4d3f245ebad549efd782fe6c00
ADMIN <ADMIN@DESKTOP-58F0PJV>
2024-11-01 14:05:13 +0530

    new demo

---
+++ demo3/src/main/java/testDemo/AA.java
@@ -0,0 +1,14 @@
+
+package testDemo;
+
+public class AA {
+
+	public static void main(String[] args) {
+		System.out.println("I am in demo3 project");
+		System.out.println("I am in demo3 project 2nd commit");
+		
+	    System.out.println("person B code"); 
+		  System.out.println("person B code"); 
+	}
+
+}
